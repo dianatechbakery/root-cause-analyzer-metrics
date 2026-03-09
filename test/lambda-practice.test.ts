81686b83aeccb4a55a254cf2b111c6540dc8e5f0
Test User <test@example.com>
2026-03-09 08:42:26 -0700

    Initial commit

---
+++ test/lambda-practice.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaPractice from '../lib/lambda-practice-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-practice-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaPractice.LambdaPracticeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
